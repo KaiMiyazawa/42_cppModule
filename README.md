e668ed738f389d575b9641563ce01c2520b5cc4e
Kai Miyazawa <miyazawa.kai.nekochan222@gmail.com>
2025-02-23 19:21:36 +0900

    fix cppModule09/ex00/BitcoinExchange.cpp

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ C++の勉強の記録です。
 ## cppModule00
 ###ex00
 
+
+
+
+
+# memo
+- https://w.wiki/DAqu
+- cpp09/ex02 https://oeis.org/A001768
--- cppModule09/ex00/BitcoinExchange.cpp
+++ cppModule09/ex00/BitcoinExchange.cpp
@@ -30,10 +30,6 @@ bool dateFormatCheck(const std::string &date)
 
 bool dateValidCheck(const std::string &date)
 {
-	// int year = std::stoi(date.substr(0, 4));
-	// int month = std::stoi(date.substr(5, 2));
-	// int day = std::stoi(date.substr(8, 2));
-	// c++98
 	int year;
 	std::istringstream(date.substr(0, 4)) >> year;
 	int month;
@@ -47,8 +43,18 @@ bool dateValidCheck(const std::string &date)
 		return true;
 	if (day < 1 || day > 31)
 		return true;
-	if (month == 2 && day > 28)
-		return true;
+	if (month == 2) {
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+			if (day > 29)
+				return true;
+		} else {
+			if (day > 28)
+				return true;
+		}
+	} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+		if (day > 30)
+			return true;
+	}
 	return false;
 }
 
